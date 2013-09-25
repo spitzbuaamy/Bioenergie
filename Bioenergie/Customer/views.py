00ba8ecaaaea52d501e7fc5799680b35f9601eea
Fabian Schauer <20092097@atn.ac.at>
2013-09-25 14:08:26 +0200

    Startapps anlegen 2

---
+++ Bioenergie/Customer/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from Abrechnung.models import Customer
+from Customer.forms import CustomerForm
+from django.core.urlresolvers import reverse_lazy
+
+
+class CustomerListView(ListView):
+    model = Customer
+    context_object_name = 'customers'
+
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    context_object_name = 'customer'
+
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    context_object_name = 'customer'
+    form_class = CustomerForm
+    #success_url = reverse_lazy('customer_list')
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    context_object_name = 'customer'
+    form_class = CustomerForm
+    # success_url = reverse_lazy('customer_list')
+
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    context_object_name = 'customer'
+    success_url = reverse_lazy('customer_list')

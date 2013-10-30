bf2392a8ad5c938a4d0a9ce17ea4c38e9b5444c7
Fabian <20092097@atn.ac.at>
2013-10-30 16:14:10 +0100

    Datenbank angepasst: Models geändert/hinzugefügt Neue Startapps
    forms.py und urls.py erstellt
    Bis inklusive ConnectionFlatRate forms.py, urls.py und views.py angepasst

---
+++ Bioenergie/BasicPrice/Forms.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.forms import ModelForm
+from Abrechnung.models import BasicPrice
+
+
+class BasicPriceForm(ModelForm):
+
+    class Meta:
+        model = BasicPrice

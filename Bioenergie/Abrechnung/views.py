7ac3dd6e0f6d13d44500563eaa2225c0f2c2b0e1
Fabian <20092097@atn.ac.at>
2013-12-11 13:40:35 +0100

    Oberfläche auf Deutsch umgeschrieben

--- Bioenergie/Abrechnung/views.py
+++ Bioenergie/Abrechnung/views.py
@@ -36,7 +36,7 @@ def user_login(request):
         else:
             # Bad login details were provided. So we can't log the user in.
             print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Username or password incorrect!")
+            return HttpResponse("Username oder Passwort falsch!")
 
 
     # The request is not a HTTP POST, so display the login form.
